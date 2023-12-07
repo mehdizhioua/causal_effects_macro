ca9c1ca019258964e8def7c4ba62b2b4dfd2fb83
Mehdi Zhioua <mehdizhioua@hotmail.com>
2023-12-06 22:50:03 -0800

    update estimator

--- estimator.py
+++ estimator.py
@@ -1,43 +1,46 @@
-import pandas as pd
-
 class Estimator:
     def __init__(self, W, Y):
         self.W = W
         self.Y = Y
 
+    def _calculate_F(self, w, h, t_p):
+        """
+        Calculate the empirical probability F that W is between w-h and w+h
+        up to time t-p, using the historical data.
+        """
+        # Use data only up to t_p (not inclusive) to ensure it's not forward looking
+        historical_data = self.W.iloc[:t_p]
+        return ((historical_data >= w - h) & (historical_data <= w + h)).mean()
+
     def estimate(self, w, w_prime, p, h):
         T = len(self.Y)
+        # Pre-calculate the empirical probabilities for all t-p to avoid looking ahead
+        F_w = [self._calculate_F(w, h, t_p) for t_p in range(p, T)]
+        F_w_prime = [self._calculate_F(w_prime, h, t_p) for t_p in range(p, T)]
+        
+        # Calculate the indicators
         indicator_w = ((self.W >= (w - h)) & (self.W <= (w + h))).astype(int)
         indicator_w_prime = ((self.W >= (w_prime - h)) & (self.W <= (w_prime + h))).astype(int)
 
-        # Calculate Ft-p for each t
-        Ft_p = self.W.rolling(window=p).mean().shift(-p)
-        
-        # Calculate ft-p(W_t-p) for each t
-        ft_p = self.W.diff(periods=p).shift(-p)
-        
-        # Nonparametric estimator for tau
-        tau_numerator = ((self.Y - self.Y.shift(p)) * (indicator_w - indicator_w_prime) /
-                         (Ft_p + h - Ft_p - h)).sum()
-        tau_hat = (1 / (T - p)) * tau_numerator
+        # Calculate the shifted Y values once for efficiency
+        Y_shifted = self.Y.shift(p)
 
-        # Variance estimator for tau
-        var_numerator = (((self.Y - self.Y.shift(p))**2) * (indicator_w + indicator_w_prime) /
-                         ft_p**2).sum()
-        v_k_squared = (1 / (T - p)) * var_numerator
-        var_tau_hat = (h**-1) * (v_k_squared / (T - p))
+        # Nonparametric estimator for tau
+        tau_sum = 0
+        var_sum = 0
+        for t_p in range(p, T):
+            tau_numerator = ((self.Y.iloc[t_p] - Y_shifted.iloc[t_p]) * 
+                             (indicator_w.iloc[t_p] - indicator_w_prime.iloc[t_p]) / 
+                             (F_w[t_p-p] - F_w_prime[t_p-p]))
+            tau_sum += tau_numerator
+            
+            # Variance estimator for tau
+            var_numerator = (((self.Y.iloc[t_p] - Y_shifted.iloc[t_p])**2) * 
+                             (indicator_w.iloc[t_p] + indicator_w_prime.iloc[t_p]))
+            var_sum += var_numerator
+
+        tau_hat = tau_sum / (T - p)
+        v_k_squared = var_sum / (T - p)
+        var_tau_hat = (h**-1) * v_k_squared / (T - p)
 
         return tau_hat, var_tau_hat
-
-# Example usage:
-# Assume we have dataframes W_df and Y_df corresponding to W_t and Y_t
-# W_df = pd.DataFrame(...)
-# Y_df = pd.DataFrame(...)
-
-# Initialize the estimator with dataframes
-# estimator = Estimator(W_df, Y_df)
-
-# Calculate the estimator and variance
-# tau_hat, var_tau_hat = estimator.estimate(w=..., w_prime=..., p=..., h=...)
-
-# Since we don't have actual data to work with, we cannot run this code here.
