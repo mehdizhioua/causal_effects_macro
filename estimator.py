e0915b94cb5388aab1e86d2d025b8fd6ea385310
Mehdi Zhioua <mehdizhioua@hotmail.com>
2023-12-08 13:26:17 -0800

    add comments

--- estimator.py
+++ estimator.py
@@ -11,25 +11,54 @@ class Estimator:
 
 
     def empirical_probability(self, w, h, t_p):
+        """
+        :param w: value of the treatment at time t_p
+        :param h: bandwidth
+        :param t_p: time at which the treatment is observed
+        :return: empirical probability of the treatment being in the interval [w-h, w+h] at time t_p
+        """
         data_up_to_t_p = self.W.loc[:t_p]
         p_w_h = np.mean((data_up_to_t_p[self.treatment] > w - h) & (data_up_to_t_p[self.treatment] <= w + h))
         return p_w_h
 
     def cumulative_distribution_function(self, w, t):
+        """
+        :param w: value of the treatment at time t
+        :return: empirical probability of the treatment being less than or equal to w at time t
+        """
         data_up_to_t = self.W.loc[:t]
         cdf_w = np.mean(data_up_to_t[self.treatment] <= w)
         return cdf_w
 
     def indicator_function(self, w, h, t):
+        """
+        :param w: value of the treatment at time t
+        :param h: bandwidth
+        :param t: time at which the treatment is observed
+        :return: indicator function of the treatment being in the interval [w-h, w+h] at time t
+        """
         W_t = self.W.loc[t, self.treatment]
         indicator_w = 1 if (W_t > w - h) and (W_t <= w + h) else 0
         return indicator_w
 
     def find_closest_time(self, t, y_time_index):
+        """
+        :param t: time at which the treatment is observed
+        :param y_time_index: time index of the outcome
+        :return: closest time in y_time_index to t
+        """
         closest_index = y_time_index.get_indexer([t], method='nearest')[0]
         return y_time_index[closest_index]
 
     def compute_sum_element(self, t_p, p, h, w, w_prime):
+        """
+        :param t_p: time at which the treatment is observed
+        :param p: lag
+        :param h: bandwidth
+        :param w: value of the treatment at time t_p
+        :param w_prime: value of the treatment at time t_p - p
+        :return: sum element of the estimator and sum element of the variance
+        """
         t = t_p + pd.DateOffset(days=p)
         t_p_1 = t_p - pd.DateOffset(days=1)
         
@@ -58,6 +87,14 @@ class Estimator:
         return sum_element_tau, sum_element_var
 
     def compute_estimator(self, p, h, w, w_prime, min_obs=10):
+        """
+        :param p: lag
+        :param h: bandwidth
+        :param w: value of the treatment at time t_p
+        :param w_prime: value of the treatment at time t_p - p
+        :param min_obs: minimum number of observations to compute the estimator
+        :return: estimator and variance
+        """
         sum_elements_tau = []
         sum_elements_var = []
         for t_p in self.W.index[min_obs:]:
@@ -70,16 +107,22 @@ class Estimator:
         return tau, var
 
     def plot_effect(self, h, w, w_prime, p_range, min_obs=10):
+        """
+        :param h: bandwidth
+        :param w: value of the treatment at time t_p
+        :param w_prime: value of the treatment at time t_p - p
+        :param p_range: range of lags
+        :param min_obs: minimum number of observations to compute the estimator
+        :return: plot of the estimator and its confidence interval as a function of the lag
+        """
         estimators = []
         stds = []
         for p in p_range:
             estimator,var= self.compute_estimator(p, h, w, w_prime, min_obs)
             estimators.append(estimator)
             stds.append(np.sqrt(var))
-
         estimators = np.array(estimators)
         stds = np.array(stds)
-
         plt.figure(figsize=(10, 6))
         plt.plot(p_range, estimators, marker='o', label='Estimator')
         plt.plot(p_range, estimators + stds, linestyle='--', color='grey', label='Estimator + 1 STD')
